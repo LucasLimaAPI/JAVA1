fa5116ae96bc80af359114f24d6101f713c64b53
Lucas Lima <llima3780@gmail.com>
2022-01-23 01:23:35 -0300

    first commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,7 @@
+public class PrimeiroPrograma{
+    public static void main(String[] args) {
+        int a = 22;
+        int b = 22;
+        System.out.println("Olá pessoal do github essa é minha primeira experiencia no JAVA!!" +" "+ (a+b));
+    }
+}
