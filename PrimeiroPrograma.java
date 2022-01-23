7d880060ecca02fac2b208320ccaaec06bd2ac1a
Lucas Lima <llima3780@gmail.com>
2022-01-23 01:56:19 -0300

    JAVA

--- PrimeiroPrograma.java
+++ PrimeiroPrograma.java
@@ -3,5 +3,26 @@ public class PrimeiroPrograma{
         int a = 22;
         int b = 22;
         System.out.println("Olá pessoal do github essa é minha primeira experiencia no JAVA!!" +" "+ (a+b));
+        livros livros = new livros();
+        System.out.println(livros);
+
+
     }
 }
+
+class livros{
+    private String nome;
+
+    public livros() {
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+}
+
+
