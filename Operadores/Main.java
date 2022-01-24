e6841ec2c59c7f6c3902ad5ae124f99712a686f6
Lucas Lima <llima3780@gmail.com>
2022-01-24 00:11:27 -0300

    JAVA

---
+++ Operadores/Main.java
@@ -0,0 +1,68 @@
+package Operadores;
+
+public class Main {
+    
+    public static void main(String[]args){
+
+        int i1 = 10;
+        int i2 = 20;
+        float f1 = 1.4f;
+        float f2 = 4.6f;
+        double d1 = 5.6;
+        char c1 ='x';
+        char c2 ='y';
+        String s1 = "Lucas";
+        String s2 = "Lima";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 8768988;
+        long l2 = 54687492;
+        byte y1 = 1;
+        short n1 = 25;
+
+        System.out.println(i1 == i2);
+        System.out.println(i1 != i2);
+        System.out.println(i1 <= i2);
+        System.out.println(i1 >= i2);
+
+        System.out.println(f1 == f2);
+        System.out.println(f1 != f2);
+        System.out.println(f1 <= f2);
+        System.out.println(f1 >= f2);
+
+        System.out.println(d1 == f2);
+        System.out.println(d1 != f2);
+        System.out.println(d1 <= f2);
+        System.out.println(d1 >= f2);
+
+        System.out.println(c1 == c2);
+        System.out.println(c1 != c2);
+        System.out.println(c1 <= c2);
+        System.out.println(c1 >= c2);
+
+        System.out.println(s1 + s2);
+        System.out.println(s1 = s2);
+        System.out.println(s1 != s2);
+        System.out.println(s1 == s2);
+
+        System.out.println(b1 = b2);
+        System.out.println(b1 == b2);
+        System.out.println(b1 == b2);
+        System.out.println(b1 != b2);
+
+        System.out.println(l1 == l2);
+        System.out.println(l1 != l2);
+        System.out.println(l1 <= l2);
+        System.out.println(l1 >= l2);
+
+        System.out.println(y1 == n1);
+        System.out.println(y1 != n1);
+        System.out.println(y1 <= n1);
+        System.out.println(y1 >= n1);
+
+
+
+    }
+    
+}
