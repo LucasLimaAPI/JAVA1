b4b7e5af9936bafe59337a31e0d183a82a9d8a8d
Lucas Lima <llima3780@gmail.com>
2022-01-26 18:35:02 -0300

    JAVA

---
+++ src/arrays/Cosoantes.java
@@ -0,0 +1,39 @@
+package src.arrays;
+
+import java.util.Scanner;
+
+public class Cosoantes {
+
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+            String[] consoantes = new String[6];
+
+            int quantidadeConsoantes = 0;
+
+            int count = 0;
+            do {
+                System.out.println("Letra: ");
+                String letra = scan.next();
+
+                if (letra.equalsIgnoreCase("a") |
+                    (letra.equalsIgnoreCase("e")) |
+                    (letra.equalsIgnoreCase("i")) |
+                     (letra.equalsIgnoreCase("o")) |
+                    (letra.equalsIgnoreCase("u"))) {
+                    consoantes[count] = letra;
+                    quantidadeConsoantes++;
+                }
+
+                count++;
+
+            } while (count < consoantes.length);
+
+            System.out.println("Consoantes: ");
+            for (String consoante : consoantes) {
+                if (consoante != null)
+                System.out.println(consoante + " ");
+
+            }
+        }
+    }
+}
