b6ae8494f92dc818761ec128cdc979c911536f33
Lucas Lima <llima3780@gmail.com>
2022-01-26 18:47:22 -0300

    JAVA

---
+++ src/arrays/Aleatorios.java
@@ -0,0 +1,25 @@
+package src.arrays;
+
+import java.util.Random;
+
+public class Aleatorios {
+    
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] numerosAleatórios = new int[20];
+
+        for(int i =0; i <numerosAleatórios.length; i++) {
+       int numero= random.nextInt(100);
+       numerosAleatórios[i]= numero;
+        }
+
+        System.out.println("Sucessores Numeros Aleatórios: ");
+        for (int numero : numerosAleatórios) {
+            System.out.println((numero+1) + " ");
+        }
+
+        
+    }
+
+}
